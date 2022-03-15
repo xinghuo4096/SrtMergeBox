f67879273f0eb6783f7731e78616fc53f3a60e3f
xinghuo4096 <262066859@qq.com>
2022-03-15 16:45:45 +0800

    Examples of batch subtitles

--- src/movie_srt_merge.py
+++ src/movie_srt_merge.py
@@ -29,3 +29,13 @@ merge_ass_tofile(first_subtitle_fname='z:/tests/cn.srt',
                  unalign_subtitle_fname='z:/tests/new_ass.unalgin.txt',
                  ass_template_fname='indata/test_ass_template_cn_en.txt',
                  mark1='', mark2='', mini_time=Srt.MINI_MERGE_TIME)
+
+for i in range(1, 9):
+    fname1 = f'd:/test/test2/test.S01E{i:02d}.1080p.WEB.H264'
+    merge_ass_tofile(first_subtitle_fname=f'{fname1}.cn.srt',
+                     second_subtitle_fname=f'{fname1}.en.srt',
+                     new_subtitle_fname=f'{fname1}.ass',
+                     unalign_subtitle_fname=f'{fname1}.unalgin.txt',
+                     ass_template_fname='indata/test_ass_template_cn_en.txt',
+                     mark1='', mark2='', mini_time=Srt.MINI_MERGE_TIME)
+print()
